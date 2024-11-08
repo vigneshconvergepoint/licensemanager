14f114b84f0faa4cfd2a89ad98e1b3aa04adce93
Vignesh R <vignesh.r@convergepoint.com>
2024-11-08 12:13:19 +0530

    port settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // port: process.env.PORT || 8080,
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 8080,
+  },
+  build: {
+    outDir: 'dist', 
   }
 })
